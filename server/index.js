2165cd0a33af258b4994a740e7f7047ed6380ef0
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-04-19 16:06:08 -0300

    add dependencies

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
+// const express = require("express");
+// const app = express();
+
+// app.use("/", (req, res)=>{
+//     res.send("Olá Mundo");
+// })
+
+// app.listen(5000, console.log("Servidor iniciado na porta 8000"));
+
+const PORT = 8080;
+const cors = require('cors')
 const express = require("express");
+const router = require('./src/routes/router')
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
 
 app.use("/", (req, res)=>{
-    res.send("Olá Mundo");
+    res.send("Bem vindo ao Delta Games API")
 })
 
-app.listen(5000, console.log("Servidor iniciado na porta 8000"));
+app.listen(PORT, ()=>{
+    console.log(`Aplicação rodando na porta ${PORT}`)
+})
