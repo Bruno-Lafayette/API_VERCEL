39c5f78d7671a19e790c653cbeee1f1a58b33a42
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-04-20 11:48:12 -0300

    correção arquivos

--- server/index.js
+++ server/index.js
@@ -1,12 +1,3 @@
-// const express = require("express");
-// const app = express();
-
-// app.use("/", (req, res)=>{
-//     res.send("Olá Mundo");
-// })
-
-// app.listen(5000, console.log("Servidor iniciado na porta 8000"));
-
 const PORT = 8080;
 const cors = require('cors')
 const express = require("express");
