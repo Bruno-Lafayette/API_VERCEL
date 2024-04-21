48cba2830e5c95a212dd9d4da09638c446999a04
Bruno Lafayette <brunolafayette@rocketmail.com>
2024-04-21 16:48:05 -0300

    add config archive connect.js

--- server/src/database/connect.js
+++ server/src/database/connect.js
@@ -4,13 +4,16 @@ require('dotenv').config();
 const connection = {
     client: 'mysql2',
     connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
     }
 };
 
 const db = knex(connection);
-module.exports = db;
+module.exports = db
